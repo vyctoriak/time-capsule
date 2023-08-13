670af1f82f0911d636503cb5ddd4a7c35c973f40
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-13 17:58:21 +0200

    blur background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16">
-
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
       </div>
 
       {/* right */}
