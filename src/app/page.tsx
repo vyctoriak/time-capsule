4b7675c14bbe83dab5ca043f1f19438d88f3e828
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-14 17:48:50 +0200

    stripes creation with css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,22 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16 border-r border-white/10">
+        {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        {/* Stripes */}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
       </div>
 
       {/* right */}
-
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             You haven't registered any memories, Start{' '}
             <a href="" className="underline hover:text-gray-50">
-              creating now!
+              creating now
             </a>
+            !
           </p>
         </div>
       </div>
