dccfa469882b0034c5a77487840231e954dfc755
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-14 18:06:56 +0200

    background stars and change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 min-h-screen">
+    <main className="grid grid-cols-2 min-h-screen bg-[url(../assets/bg-stars.svg)] bg-cover">
       {/* left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16 border-r border-white/10">
         {/* Blur */}
@@ -10,12 +10,12 @@ export default function Home() {
       </div>
 
       {/* right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
-            You haven't registered any memories, Start{' '}
+            You haven't registered any memories, start to{' '}
             <a href="" className="underline hover:text-gray-50">
-              creating now
+              create now
             </a>
             !
           </p>
