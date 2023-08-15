4fe9d53bbbcee2e5ebbe4735f0f7adecc4f09647
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-15 17:31:29 +0200

    eslint, prettier e tailwindcss prettier plugin configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,32 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree } from 'next/font/google'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
 import { ReactNode } from 'react'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({
   subsets: ['latin'],
   weight: '700',
-  variable: '--font-bai-jamjuree'
+  variable: '--font-bai-jamjuree',
 })
 
 export const metadata: Metadata = {
   title: 'NLW Spacetime',
-  description: 'A time capsule built with React, Next.js, TailwindCSS and Typescript',
+  description:
+    'A time capsule built with React, Next.js, TailwindCSS and Typescript',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans text-gray-100 bg-gray-900`}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
