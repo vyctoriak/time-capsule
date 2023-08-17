620d432cb4999289e565418fd38d1aa12597b569
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-17 20:14:40 +0200

    home page skeleton with css and the images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2 bg-[url(../assets/bg-stars.svg)] bg-cover">
@@ -8,9 +12,45 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <a href="" className="flex items-center gap-3 text-left">
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400"></div>
+        {/* Sign in */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="text-small max-w-[150px] leading-snug">
+            <span className="underline">Create your account</span> and save
+            memories!
+          </p>
         </a>
+
+        {/* Hero */}
+
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="Logo NLW Spacetime" />
+          <div className="max-w-[520px] space-y-1">
+            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+              Your time capsule
+            </h1>
+            <p className="mt-1 text-lg leading-relaxed">
+              Collect memorable moments from your journey and share (if you
+              like) with the world!
+            </p>
+          </div>
+
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase 
+            leading-none text-black hover:bg-green-600"
+          >
+            REGISTER A MEMORY
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div>Built with &#10084;&#65039; by Vyctoria K.</div>
       </div>
 
       {/* right */}
