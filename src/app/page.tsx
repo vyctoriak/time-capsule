19598f09a5ce538fd49d281e25537be2e5ae4932
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-17 20:18:23 +0200

    home page finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,12 @@ export default function Home() {
         </div>
 
         {/* Copyright */}
-        <div>Built with &#10084;&#65039; by Vyctoria K.</div>
+        <div className="text-sm leading-relaxed text-gray-500">
+          Built with &#10084;&#65039; by{' '}
+          <span className="cursor-pointer text-green-500 underline hover:text-green-700">
+            Vyctoria K.
+          </span>
+        </div>
       </div>
 
       {/* right */}
